d1548f0accc62ebb8d63faed482156d33c856a18
Grzegorz Kowalski <grzegorz.adam.kowalski@outlook.com>
2022-09-06 22:49:19 +0200

    Support for mamba

--- webui.cmd
+++ webui.cmd
@@ -1,6 +1,15 @@
 @echo off
 
 set conda_env_name=ldo
+set conda_command=conda
+
+set use_mamba_instead_of_conda=1
+if "%use_mamba_instead_of_conda%"=="1" (
+  where /q mamba
+  if errorlevel 0 echo mamba installed
+  if errorlevel 1 conda install mamba -n base -c conda-forge
+  set conda_command=mamba
+)
 
 :: Put the path to conda directory after "=" sign if it's installed at non-standard path:
 set custom_conda_path=
@@ -37,17 +46,17 @@ IF "%CONDA_PATH%"=="" (
 
 :foundPath
 call "%CONDA_PATH%\Scripts\activate.bat"
-conda env list | findstr /r /b /c:"%conda_env_name%"
+%conda_command% env list | findstr /r /b /c:"%conda_env_name%"
 if errorlevel 0 goto :conda_env_update
 if errorlevel 1 goto :conda_env_create
 goto :eof
 
 :conda_env_create
-conda env create --name "%conda_env_name%" --file environment.yaml
+call %conda_command% env create --name "%conda_env_name%" --file environment.yaml
 goto :conda_activate
 
 :conda_env_update
-conda env update --name "%conda_env_name%" --file environment.yaml --prune
+call %conda_command% env update --name "%conda_env_name%" --file environment.yaml --prune
 goto :conda_activate
 
 :conda_activate
